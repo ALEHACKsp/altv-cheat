604af88af058e8d37986c6f9422c79f9cefa815a
Keiichi32 <44448654+Keiichi32@users.noreply.github.com>
2020-11-12 21:52:13 +0100

    Add files via upload

---
+++ altv-cheat/Cheat.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Executor
+{
+    void Initialize();
+    void File(std::string const& str);
+    void Find(std::string const& str);
+    void Remove(std::string const& str);
+    void Serial(std::string const& str);
+    void Weapon();
+    void Dump();
+};
